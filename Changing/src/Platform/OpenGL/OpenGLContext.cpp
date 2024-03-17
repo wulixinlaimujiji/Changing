6d04563929c9fe96328ab07919068a2821e7d2ca
ChangeeShaw <x1050692380@gmail.com>
2024-03-18 01:49:50 +0800

    Added OpenGLContext Class

---
+++ Changing/src/Platform/OpenGL/OpenGLContext.cpp
@@ -0,0 +1,28 @@
+#include "chngpch.h"
+#include "OpenGLContext.h"
+
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <GL/GL.h>
+
+namespace Changing {
+
+	OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
+		: m_WindowHandle(windowHandle)
+	{
+		CHNG_CORE_ASSERT(windowHandle, "Window handle is null!")
+	}
+
+	void OpenGLContext::Init()
+	{
+		glfwMakeContextCurrent(m_WindowHandle);
+		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+		CHNG_CORE_ASSERT(status, "Failed to initialize Glad!");
+	}
+
+	void OpenGLContext::SwapBuffers()
+	{
+		glfwSwapBuffers(m_WindowHandle);
+	}
+
+}
--- Changing/src/Platform/Windows/WindowsWindow.h
+++ Changing/src/Platform/Windows/WindowsWindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Changing/Window.h"
+#include "Changing/Renderer/GraphicsContext.h"
 
 #include <GLFW/glfw3.h>
 
@@ -28,6 +29,7 @@ namespace Changing {
 
 	private:
 		GLFWwindow* m_Window;
+		GraphicsContext* m_Context;
 
 		struct WindowData
 		{
