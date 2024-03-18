27f6870bae6dd92b6ba3260c8842a43d74ffe6bf
ChangeeShaw <x1050692380@gmail.com>
2024-03-18 08:24:06 +0800

    Added BufferLayout Class

--- Changing/src/Platform/OpenGL/OpenGLContext.cpp
+++ Changing/src/Platform/OpenGL/OpenGLContext.cpp
@@ -20,9 +20,12 @@ namespace Changing {
 		CHNG_CORE_ASSERT(status, "Failed to initialize Glad!");
 
 		CHNG_CORE_INFO("OpenGL Info:");
-		/*CHNG_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
-		CHNG_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
-		CHNG_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));*/
+		std::string vendor = reinterpret_cast<const char*>(glGetString(GL_VENDOR));
+		CHNG_CORE_INFO("  Vendor: {}", vendor);
+		std::string renderer = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+		CHNG_CORE_INFO("  Renderer: {}", renderer);
+		std::string version = reinterpret_cast<const char*>(glGetString(GL_VERSION));
+		CHNG_CORE_INFO("  Version: {}", version);
 	}
 
 	void OpenGLContext::SwapBuffers()
